183d5bf807c041a0c3923294aa106270305d524d
upedro <p.marques.dev@gmail.com>
2022-01-15 15:32:57 -0300

    Ajuste de layout e cores

--- src/components/Tabela/style.ts
+++ src/components/Tabela/style.ts
@@ -5,5 +5,6 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
     overflow-y: scroll;
     display: flex;
-    justify-content: center;
+    justify-content: start;
+    min-width: 100%;
 `
